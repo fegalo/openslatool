730ad4f701c0230b2535ac0927820d96ba4e6db2
Fermin Gallego <fegalodev@outlook.com>
2019-02-24 22:16:07 +0100

    Sample with charts

--- js/main.js
+++ js/main.js
@@ -12,6 +12,7 @@ var incidents_list = [];
 function main() {
   create_default_list();
   fill_table(incidents_list);
+  create_sample_chart();//sample
 }
 
 /** Create a default list of incidents */
@@ -100,5 +101,45 @@ function getValue(id){
     value=$(id).val()
   }
   return value;
+}
+//charts
+function create_sample_chart(){
+var ctx = document.getElementById("myChart").getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["Back", "Middle", "Front", "Network", "Release", "Capacity"],
+        datasets: [{
+            label: '# of Incidents',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
 }
 
