2d7e4f1d8736eca1b9af0b79c2501308ab3a810b
Fermin Gallego <fegalodev@outlook.com>
2019-02-26 20:48:14 +0100

    Chart with incidents per tag

--- js/main.js
+++ js/main.js
@@ -44,6 +44,7 @@ function load_events() {
   });
   $('#calcButton').on('click',function() {
    fill_ava(incidents_list);
+   fill_charts(incidents_list);
   });
   $('#inputIncidentListFile').on('change',function(e) {
    handle_file(e);
@@ -77,6 +78,19 @@ function fill_ava(list){
   $('#maxDTime').val(kpis.max_dtime);
   $('#avail').val(kpis.availability + '%');
 }
+/** Fill charts*/
+function fill_charts(list){
+  if(!$('#chartsDiv').hasClass('show'))
+  {
+    sla_log_debug('chartsDiv hidden')
+    return;
+  }
+  var year=getValue('#year');
+  var month=getValue('#month');
+
+  var tags_map=sla_calc_tags(list,month);
+  create_chart(tags_map);
+}
 /** Handle files */
 function handle_file(evt) {
   var files = evt.target.files;
@@ -100,6 +114,48 @@ function getValue(id){
     value=$(id).val()
   }
   return value;
+}
+//charts
+function create_chart(tags){
+var ctx = document.getElementById("myChart").getContext('2d');
+var tag_entries =  Array.from(tags.entries());
+var tag_values =  Array.from(tags.values());
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: tag_entries,
+        datasets: [{
+            label: '# of Incidents',
+            data: tag_values,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
 }
 //charts
 function create_sample_chart(){
