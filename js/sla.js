56eca754ba7830fc3545f6d592be94c57c1bb687
Fermin Gallego <fegalodev@outlook.com>
2019-03-07 22:45:26 +0100

    Service Window

--- js/sla.js
+++ js/sla.js
@@ -13,13 +13,24 @@ function sla_log_debug(msg){
   }
   console.log( format, 'color: yellow; background-color: black;', sla_log_debug.caller.name, msg);
 }
-/**
-Calculate the total minutes of the month
-*/
+
 function sla_calc_month_time(year,month){
   var start = moment(year+'-'+month+'-01 00:00');
   var end=moment(start).add(1, 'M');
   var tmonth = end.diff(start, 'minutes')
+  sla_log_debug(tmonth);
+  return tmonth;
+}
+
+function sla_calc_month_time_sw(year,month){
+  var start = moment(year+'-'+month+'-01 00:00');
+  var end=moment(start).add(1, 'M');
+  var tmonth = end.diff(start, 'minutes')*19/24;
+  if(month==3){
+    tmonth+=60*19/24;
+  }else if (month==10){
+    tmonth-=60*19/24;
+  }
   sla_log_debug(tmonth);
   return tmonth;
 }
@@ -28,29 +39,40 @@ Calculate the total minutes of the incidents
 */
 function sla_calc_kpis(list,tag,subtag,month,year){
   var total_dtime=0;
+  var total_dtime_sw=0;
   var num_inc=0;
   var max_dtime=0;
   var total_month = sla_calc_month_time(year,month);
+  var total_month_sw = sla_calc_month_time_sw(year,month);
   list.forEach(function(e) {
     if(($.inArray(tag,e.tags) != -1)
         &&(moment(e.start).month()==month-1)
         &&( ($.inArray(subtag,e.subtags) != -1) || ("*" === subtag ))) {
+      //24x7
       var duration_time=sla_extract_time(e);
       total_dtime+=duration_time;
       num_inc++;
       if(duration_time>max_dtime){
         max_dtime=duration_time;
       }
+      //sw
+      var duration_time_sw=sla_extract_time_sw(e);
+      total_dtime_sw+=duration_time_sw;
     }
   });
   var ava= 1-total_dtime/total_month;
   ava=Math.round(ava * 100 * 1000) / 1000;
+  var avasw= 1-total_dtime_sw/total_month_sw;
+  avasw=Math.round(avasw * 100 * 1000) / 1000;
   var ret={
     "total_dtime": total_dtime,
     "total_month": total_month,
+    "total_dtime_sw": total_dtime_sw,
+    "total_month_sw": total_month_sw,
     "num_inc": num_inc,
     "max_dtime": max_dtime,
-    "availability": ava
+    "availability": ava,
+    "availabilitysw": avasw
   };
   sla_log_debug(ret);
   return ret;
@@ -114,5 +136,34 @@ Calculate the duration of an incident
 function sla_extract_time(inc){
   var start = moment(inc.start);
   var end = moment(inc.end);
+  var diff=0;
   return end.diff(start, 'minutes')
+}
+function sla_extract_time_sw(inc){
+  var start=moment(inc.start);
+  var end = moment(inc.end);
+  var diff=0;
+  if(start.hours()<1){
+    if(end.hours()<6){
+      end=end.hours(1).minutes(0);
+      diff=end.diff(start, 'minutes');
+    }else{
+      var end1=end.clone();
+      end1=end1.hours(1).minutes(0);
+      diff=end1.diff(start, 'minutes');
+
+      start=start.hours(6).minutes(0);
+      diff+=end.diff(start, 'minutes');
+    }
+  }else if(start.hours()>1 && start.hours()<6){
+    if(end.hours()<6){
+      diff=0;
+    }else{
+      start=start.hours(6).minutes(0);
+      diff=end.diff(start, 'minutes');
+    }
+  }else if(start.hours()>=6){
+    diff=end.diff(start, 'minutes')
+  }
+  return diff;
 }
